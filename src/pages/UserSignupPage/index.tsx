4c54acab1588c4b5fc17848ba0c4022142ac86f5
Mariana Rossi <wmarianarossi@gmail.com>
2024-11-19 22:57:40 -0300

    adding initial react files

---
+++ src/pages/UserSignupPage/index.tsx
@@ -0,0 +1,14 @@
+export function UserSignupPage ()
+{
+    return (
+        <>
+        <main className="form-signup w-100 m-auto">
+            <form>
+                <div>
+                    
+                </div>
+            </form>
+        </main>
+        </>
+    )
+}
